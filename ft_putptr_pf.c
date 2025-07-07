28c43bbfe84956338ed9458af46e8d223c8bb450
Pedro Fousianis <toftyper@gmail.com>
2025-07-07 20:39:58 +0200

    Update ft_putptr_pf.c

--- ft_putptr_pf.c
+++ ft_putptr_pf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putptr_pf.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pedfousi <pedfousi@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: pedfousi <pedfousi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 16:53:40 by pedfousi          #+#    #+#             */
-/*   Updated: 2025/05/10 16:53:41 by pedfousi         ###   ########.fr       */
+/*   Updated: 2025/07/07 20:37:41 by pedfousi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void	ft_putptr_pf(void *ptr, size_t *counter)
 	char			*str;
 	unsigned long	ptr_address;
 
+	if (ptr == NULL)
+	{
+		ft_putstr_pf("(nil)", counter);
+		return ;
+	}
 	ptr_address = (unsigned long)ptr;
 	ft_putstr_pf("0x", counter);
 	str = ft_aux_pf(ptr_address, HEX_LOW_BASE);
