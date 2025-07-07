f1451f2bc241e778fd774d9580e53ff0bfa69767
Pedro Fousianis <toftyper@gmail.com>
2025-07-07 20:40:36 +0200

    Update main.c

--- main.c
+++ main.c
@@ -10,17 +10,66 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
-{
-	int		number;
-	char	*pointer;
+#include "ft_printf.h"
+#include <stdio.h> // Para usar printf
 
-	number = -150;
-	*pointer = "little";
-	printf("Here is a number-%4d-and a-%10s-word.\n", number, pointer);
-	printf(" %-10d \n", number);
-	printf(" %010d \n", number);
-	printf(" %-#10x \n", number);
-	printf(" %#x \n", number);
-	printf(" %.d \n", number);
+int main(void)
+{
+    int count_ft;
+    int count_std;
+
+    // Prueba con cadenas simples
+    count_ft = ft_printf("Hola, mundo!\n");
+    count_std = printf("Hola, mundo!\n");
+    ft_printf("ft_printf caracteres impresos: %d\n", count_ft);
+    printf("printf caracteres impresos: %d\n", count_std);
+
+    // Comparación con diferentes formatos
+    ft_printf("Caracter: %c\n", 'A');
+    printf("Caracter: %c\n", 'A');
+
+    ft_printf("Cadena: %s\n", "Esto es una prueba");
+    printf("Cadena: %s\n", "Esto es una prueba");
+
+    ft_printf("Puntero: %p\n", (void *)main);
+    printf("Puntero: %p\n", (void *)main);
+
+    ft_printf("Entero: %d\n", 42);
+    printf("Entero: %d\n", 42);
+
+    ft_printf("Unsigned: %u\n", 123456);
+    printf("Unsigned: %u\n", 123456);
+
+    ft_printf("Hexadecimal (minúsculas): %x\n", 255);
+    printf("Hexadecimal (minúsculas): %x\n", 255);
+
+    ft_printf("Hexadecimal (mayúsculas): %X\n", 255);
+    printf("Hexadecimal (mayúsculas): %X\n", 255);
+
+    ft_printf("Porcentaje: %%\n");
+    printf("Porcentaje: %%\n");
+
+    // Pruebas límite
+    ft_printf("Cadena vacía: %s\n", "");
+    printf("Cadena vacía: %s\n", "");
+
+    ft_printf("Puntero NULL: %p\n", NULL);
+    printf("Puntero NULL: %p\n", NULL);
+
+    ft_printf("Entero mínimo: %d\n", -2147483648);
+    printf("Entero mínimo: %d\n", -2147483648);
+
+    ft_printf("Entero máximo: %d\n", 2147483647);
+    printf("Entero máximo: %d\n", 2147483647);
+
+    ft_printf("Unsigned máximo: %u\n", 4294967295);
+    printf("Unsigned máximo: %u\n", 4294967295);
+
+    ft_printf("Hexadecimal máximo (minúsculas): %x\n", 4294967295);
+    printf("Hexadecimal máximo (minúsculas): %x\n", 4294967295);
+
+    ft_printf("Hexadecimal máximo (mayúsculas): %X\n", 4294967295);
+    printf("Hexadecimal máximo (mayúsculas): %X\n", 4294967295);
+
+    return 0;
 }
